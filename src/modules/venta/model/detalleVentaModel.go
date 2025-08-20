d8bbd714b7f32909bd5ed55bf8e15c5dccfc1853
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-08-20 16:48:29 -0400

    cambios

--- src/modules/venta/model/detalleVentaModel.go
+++ src/modules/venta/model/detalleVentaModel.go
@@ -8,13 +8,13 @@ import (
 )
 
 type DetalleVentaModel struct {
-	ID             bson.ObjectID `bson:"_id,omitempty"`
-	Producto       bson.ObjectID `bson:"producto"`
-	Venta          bson.ObjectID `bson:"venta"`
-	Cantidad       int           `bson:"cantidad"`
-	PrecioUnitario float64       `bson:"precioUnitario"`
-	preciototal    float64       `bson:"preciototal"`
-	Flag           enum.Estado   `bson:"flag"`
-	Fecha          time.Time     `bson:"fecha"`
-	Descripcion    string        `bson:"descripcion"`
+	ID          bson.ObjectID `bson:"_id,omitempty"`
+	Producto    bson.ObjectID `bson:"producto"`
+	Stock       bson.ObjectID `bson:"stock"`
+	Venta       bson.ObjectID `bson:"venta"`
+	Cantidad    int           `bson:"cantidad"`
+	preciototal float64       `bson:"preciototal"`
+	Flag        enum.Estado   `bson:"flag"`
+	Fecha       time.Time     `bson:"fecha"`
+	Descripcion string        `bson:"descripcion"`
 }
