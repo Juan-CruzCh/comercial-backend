af6b9ba6a798b72dcbf20802332825d91d74dfb5
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-08-20 12:33:45 -0400

    cambios

--- src/modules/venta/model/ventaModel.go
+++ src/modules/venta/model/ventaModel.go
@@ -1,1 +1,20 @@
 package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/v2/bson"
+)
+
+type VentaModel struct {
+	ID         bson.ObjectID `bson:"_id,omitempty"`
+	Codigo     string        `bson:"codigo"`
+	Total      float64       `bson:"total"`
+	FechaVenta time.Time     `bson:"fechaVenta"`
+	Fecha      time.Time     `bson:"fecha"`
+	Flag       bool          `bson:"flag"`
+	Estado     string        `bson:"estado"`
+	TipoPago   string        `bson:"tipoPago"`
+	Usuario    bson.ObjectID `bson:"usuario"`
+	Sucursal   bson.ObjectID `bson:"sucursal"`
+}
