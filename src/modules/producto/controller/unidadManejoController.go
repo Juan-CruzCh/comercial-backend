6cc59652741a39ce078ab912fc271a987e5ee7de
Juan carlos chocllu <j.chocllu@opti-system.com>
2025-08-20 18:16:53 -0400

    cambios

--- src/modules/producto/controller/unidadManejoController.go
+++ src/modules/producto/controller/unidadManejoController.go
@@ -1,9 +1,38 @@
 package controller
 
-import "github.com/gin-gonic/gin"
+import (
+	"comercial-backend/src/modules/producto/dto"
+	"comercial-backend/src/modules/producto/service"
+	"context"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
 
 func CrearUnidadController(c *gin.Context) {
+	validate := validator.New()
+	var body dto.UnidadManejoDto
+	ctx, cancel := context.WithTimeout(c.Request.Context(), 10*time.Second)
+	defer cancel()
 
+	err := c.ShouldBindJSON(&body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	err = validate.Struct(body)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	err = service.CrearUnidadManejoService(&body, ctx)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+		return
+	}
+	c.JSON(http.StatusCreated, gin.H{"status": http.StatusCreated})
 }
 
 func ListarUnidadesController(c *gin.Context) {
--- src/modules/producto/service/unidadManejoService.go
+++ src/modules/producto/service/unidadManejoService.go
@@ -1,11 +1,28 @@
 package service
 
 import (
+	"comercial-backend/src/core/config"
+	"comercial-backend/src/core/enum"
+	"time"
+
+	"comercial-backend/src/modules/producto/dto"
+	"comercial-backend/src/modules/producto/model"
 	"context"
 )
 
-func CrearUnidadManejoService() {
+func CrearUnidadManejoService(unidadManejoDto *dto.UnidadManejoDto, ctx context.Context) error {
+	collection := config.MongoDatabase.Collection("UnidadManejo")
+	var model = model.UnidadManejoModel{
+		Nombre: unidadManejoDto.Nombre,
+		Fecha:  time.Now(),
+		Flag:   enum.EstadoNuevo,
+	}
+	_, err := collection.InsertOne(ctx, model)
+	if err != nil {
 
+		return err
+	}
+	return nil
 }
 
 func ObtenerUnidadManejoPorIDService(ctx context.Context) {
