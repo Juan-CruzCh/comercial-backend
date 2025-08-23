1089e920a506a63b58e0083187e31a30461772cd
Juan-CruzCh <kimysheeran@gmail.com>
2025-08-22 23:18:49 -0400

    cambios

--- src/modules/stock/dto/stockDto.go
+++ src/modules/stock/dto/stockDto.go
@@ -7,7 +7,7 @@ import (
 type IngresoStockData struct {
 	Proveedor  string     `json:"proveedor"  binding:"required"`
 	Factura    string     `json:"factura" binding:"required"`
-	MontoTotal float64    `json:"montoTotal"  binding:"required,gte=0"`
+	MontoTotal float64    `json:"montoTotal"  binding:"gte=0"`
 	Stock      []StockDto `json:"stock"  binding:"required,dive,required"`
 }
 
@@ -15,8 +15,8 @@ type StockDto struct {
 	Cantidad         int       `json:"cantidad" binding:"required,gt=0"`
 	FechaVencimiento time.Time `json:"fechaVencimiento" binding:"omitempty"`
 	Producto         string    `json:"producto"  binding:"required"`
-	PrecioUnitario   float64   `json:"precioUnitario" binding:"required,gte=0"`
-	MontoTotal       float64   `json:"montoTotal" binding:"required,gte=0"`
-	Descuento        float64   `json:"descuento" binding:"required,gte=0"`
-	SudTotal         float64   `json:"sudTotal" binding:"required,gte=0"`
+	PrecioUnitario   float64   `json:"precioUnitario" binding:"gte=0"`
+	MontoTotal       float64   `json:"montoTotal" binding:"gte=0"`
+	Descuento        float64   `json:"descuento" binding:"gte=0"`
+	SudTotal         float64   `json:"sudTotal" binding:"gte=0"`
 }
